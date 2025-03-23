f9fef9489dd3f30e1bff58d0d9c1fa1998907cd2
Julia Scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-03-23 16:28:18 -0300

    Inicializa a agenda de contatos com estrutura básica

---
+++ agenda.py
@@ -0,0 +1,12 @@
+def exibir_menu():
+    print("""
+    AGENDA DE CONTATOS
+    1. Adicionar contato
+    2. Listar contatos
+    3. Buscar contato
+    4. Atualizar contato
+    5. Remover contato
+    6. Favoritar/Desfavoritar contato
+    7. Listar contatos favoritos
+    8. Sair
+    """)
